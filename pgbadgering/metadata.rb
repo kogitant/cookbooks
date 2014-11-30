2b0948d5129526ed3179854e15133ee72f481bd3
Antti Koivisto <kogitant@koant.com>
2014-11-30 09:58:32 +0200

    Apache installed, pgbadger report generated under /var/wwww/pgbadger.html every 5 minutes (available at 192.168.101.103/pgbadger.html), postgres.conf tuned to log a lot in the log

--- pgbadgering/metadata.rb
+++ pgbadgering/metadata.rb
@@ -7,6 +7,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 	"apt"  
+depends 	"apache2"  
 depends 	"postgres"  
+depends 	"perl"  
+depends 	"cron"  
 depends 	"database"
 depends 	"chef-pgbadger-latest"
--- pgbadgering/recipes/default.rb
+++ pgbadgering/recipes/default.rb
@@ -12,6 +12,8 @@ ENV["LANGUAGE"] = ENV["LANG"] = ENV["LC_ALL"] = "fi_FI.UTF-8"
 include_recipe "postgresql::server"
 include_recipe "database::postgresql"
 include_recipe "chef-pgbadger-latest::default"
+include_recipe "apache2"
+include_recipe "cron"
 	
 postgresql_connection_info = {:host => "localhost",
                               :port => node['postgresql']['config']['port'],
@@ -34,4 +36,13 @@ end
 
 apt_package "screen" do
     action :install
+end
+
+apache_site 'default' do
+  enable true
+end
+
+cron "pgbadger" do
+  minute '*/5'
+  command "/opt/pgbadger/pgbadger /var/log/postgresql/postgresql-9.1-main.log -O /var/www -o pgbadger.html"
 end
